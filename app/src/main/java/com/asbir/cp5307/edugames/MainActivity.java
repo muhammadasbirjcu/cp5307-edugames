a86d77e7b8b24e69cafaa68a7cbbeb28b42908d3
Syar <xarasbir@gmail.com>
2022-04-24 11:15:09 +1000

    Android boilerplate

---
+++ app/src/main/java/com/asbir/cp5307/edugames/MainActivity.java
@@ -0,0 +1,14 @@
+package com.asbir.cp5307.edugames;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
